689eeb7fee4e7e8c8f396c3f46d2c88c65704b74
Handensaken <Handensaken@users.noreply.github.com>
2020-12-15 12:12:58 +0100

    initial commit

---
+++ Snek/Grid.cs
@@ -0,0 +1,19 @@
+using System;
+using Raylib_cs;
+
+namespace Snek
+{
+    public class Grid
+    {
+
+        int[,] grid = new int[20, 20];
+
+        public void Draw()
+        {
+            ;
+
+
+            
+        }
+    }
+}
---
+++ Snek/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Snek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800,600, "Snek");
+            Raylib.SetTargetFPS(60);
+
+            Grid grid = new Grid();
+
+            while(!Raylib.WindowShouldClose()){
+                //MATTE VRRR
+
+
+
+
+
+                //FIN BILD VRRR
+                Raylib.BeginDrawing();
+                //Raylib.ClearBackground(Color.WHITE);
+                grid.Draw();
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
